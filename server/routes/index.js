ee1b314d6c2425fe4eaf881cdf1542f2dd7b0412
Daniel Diaz Platon <daniel_dp96@hotmail.com>
2020-09-20 19:15:29 -0500

    Agregue categoria y producto

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,5 +5,7 @@ const app = express();
 // importe la ruta delusuario.js para las peticiones GET,PUT,POST,DELETE
 app.use(require('./usuario'));
 app.use(require('./login'));
+app.use(require('./categoria'));
+app.use(require('./producto'));
 
 module.exports = app;
